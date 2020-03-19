0a17b723ea0a23daf2981741ae0228a59fc0a803
Raúl Cubitto <raulcubitto@gmail.com>
2020-03-19 16:50:52 -0300

    wip

--- src/JsonPretty.php
+++ src/JsonPretty.php
@@ -4,5 +4,55 @@ namespace Rcubitto\JsonPretty;
 
 class JsonPretty
 {
-    // Build your next great package.
+    protected $fifo = []; // up
+    protected $lifo = []; // down
+
+    public static function format($sample)
+    {
+        return (new self)->process((array) $sample);
+    }
+
+    //
+
+    private function process($sample)
+    {
+        // check if starting sample it is an array or an object
+        $keys = array_keys($sample);
+
+        $isArray = false;
+        foreach ($keys as $key) {
+            if (is_integer($key)) {
+                $isArray = true;
+                break;
+            }
+        }
+
+        if ($isArray) {
+            $this->fifo[] = "<span style=\"color:black\">[</span>";
+            $this->lifo[] = "<span style=\"color:black\">]</span>";
+        } else {
+            $this->fifo[] = "<span style=\"color:black\">{</span>";
+            foreach ($sample as $key => $value) {
+                $color = is_string($value) ? 'green' : 'blue';
+                $value = is_string($value) ? "\"$value\"" : $value;
+                $this->fifo[] = str_repeat(' ', 4) . "<span style=\"color:black\">$key</span>: <span style=\"color:$color\">$value</span>";
+            }
+            $this->lifo[] = "<span style=\"color:black\">}</span>";
+        }
+
+        // keep on going!
+
+
+
+        return $this->build();
+    }
+
+    private function build()
+    {
+        return "<pre>" .
+            implode(PHP_EOL, $this->fifo) .
+            ((count($this->lifo) === 1 && count($this->fifo) === 1) ? '' : PHP_EOL) .
+            implode(PHP_EOL, array_reverse($this->lifo))
+        . "</pre>";
+    }
 }
--- tests/JsonPrettyTest.php
+++ tests/JsonPrettyTest.php
@@ -3,12 +3,59 @@
 namespace Rcubitto\JsonPretty\Tests;
 
 use PHPUnit\Framework\TestCase;
+use Rcubitto\JsonPretty\JsonPretty;
 
 class JsonPrettyTest extends TestCase
 {
     /** @test */
-    public function true_is_true()
+    function format_empty_array()
     {
-        $this->assertTrue(true);
+        $sample = [];
+        $output = '<pre><span style="color:black">{</span><span style="color:black">}</span></pre>';
+
+        $this->assertEquals($output, JsonPretty::format($sample));
+    }
+
+    /** @test */
+    function format_object_with_one_key_and_number()
+    {
+        $sample = ['a' => 1];
+
+        $output = '<pre>';
+        $output .= '<span style="color:black">{</span>' . PHP_EOL;
+        $output .= '    <span style="color:black">a</span>: <span style="color:blue">1</span>' . PHP_EOL;
+        $output .= '<span style="color:black">}</span>';
+        $output .= '</pre>';
+
+        $this->assertEquals($output, JsonPretty::format($sample));
+    }
+
+    /** @test */
+    function format_object_with_many_keys_and_numbers()
+    {
+        $sample = ['a' => 1, 'b' => 2];
+
+        $output = '<pre>';
+        $output .= '<span style="color:black">{</span>' . PHP_EOL;
+        $output .= '    <span style="color:black">a</span>: <span style="color:blue">1</span>' . PHP_EOL;
+        $output .= '    <span style="color:black">b</span>: <span style="color:blue">2</span>' . PHP_EOL;
+        $output .= '<span style="color:black">}</span>';
+        $output .= '</pre>';
+
+        $this->assertEquals($output, JsonPretty::format($sample));
+    }
+
+    /** @test */
+    function format_object_with_key_and_string()
+    {
+        $sample = ['a' => 'foo'];
+
+        $output = '<pre>';
+        $output .= '<span style="color:black">{</span>' . PHP_EOL;
+        $output .= '    <span style="color:black">a</span>: <span style="color:green">"foo"</span>' . PHP_EOL;
+        $output .= '<span style="color:black">}</span>';
+        $output .= '</pre>';
+
+        $this->assertEquals($output, JsonPretty::format($sample));
     }
 }
